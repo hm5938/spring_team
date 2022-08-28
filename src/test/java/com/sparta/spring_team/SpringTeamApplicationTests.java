6c530aac955c5057b4d7f558bfadc61fead324d5
JinKyong <sjk900700@gmail.com>
2022-08-28 14:40:33 +0900

    Initial commit

---
+++ src/test/java/com/sparta/spring_team/SpringTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
