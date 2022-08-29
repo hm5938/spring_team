b79ddd146ef716a58659c37106b62ee4736e66db
Hyemm <hmlee5938@gmail.com>
2022-08-30 07:23:05 +0900

    Create SubComment.java

---
+++ src/main/java/com/sparta/spring_team/entity/SubComment.java
@@ -0,0 +1,43 @@
+package com.sparta.spring_team.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class SubComment extends Timestamped{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @JoinColumn(name = "comment_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Comment comment;
+
+//    @JoinColumn(name = "member_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Member member;
+
+    @Column(nullable = false)
+    private String subComment;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "subcomment", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Likes> likes;
+
+    @Column()
+    private Long likeNum;
+
+
+}
