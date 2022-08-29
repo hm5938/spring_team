3faa7ddc2eb224fdc83f47eab8bf384c75d6d03b
Hyemm <hmlee5938@gmail.com>
2022-08-30 07:22:47 +0900

    Update Post.java

--- src/main/java/com/sparta/spring_team/entity/Post.java
+++ src/main/java/com/sparta/spring_team/entity/Post.java
@@ -1,50 +1,47 @@
 package com.sparta.spring_team.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Post extends Timestamped {
-
+public class Post extends Timestamped{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Member member;
+
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column()
     private String content;
 
-    @Column(nullable = true)
-    private String image;
+    @Column()
+    private String imageUrl;
 
-    //좋아요 추가
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "post", cascade = CascadeType.REMOVE)
+    @JsonIgnore
+    private List<Comment> comments;
 
-    //댓글 목록
-//    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<Comment> comments;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "post", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Likes> likes;
+    @Column()
+    private Long likeNum;
 
-    //멤버
-//    @JoinColumn(name = "member_id", nullable = false)
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    private Member member;
-//
-//    public void update(PostRequestDto postRequestDto) {
-//        this.title = postRequestDto.getTitle();
-//        this.content = postRequestDto.getContent();
-//    }
-//
-//    public boolean validateMember(Member member) {
-//        return !this.member.equals(member);
-//    }
+
 
 }
