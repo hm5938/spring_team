69e7a983f40cb1874c574a7a976f8da16ea85871
JinKyong <48237998+JinKyong@users.noreply.github.com>
2022-08-30 22:45:59 +0900

    Comment CRUD 추가
    댓글 CRUD 완성 및 추가

--- src/main/java/com/sparta/spring_team/entity/Comment.java
+++ src/main/java/com/sparta/spring_team/entity/Comment.java
@@ -1,6 +1,7 @@
 package com.sparta.spring_team.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sparta.spring_team.dto.request.CommentRequestDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,16 +20,18 @@ public class Comment extends Timestamped{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JsonIgnore
-    private Post post;
-
+    @JoinColumn(name = "member_id", nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
     @JsonIgnore
     private Member member;
 
+    @JoinColumn(name = "post_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Post post;
+
     @Column(nullable = false)
-    private String comment;
+    private String content;
 
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "comment", cascade = CascadeType.ALL)
@@ -37,6 +40,10 @@ public class Comment extends Timestamped{
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "comment", cascade = CascadeType.ALL)
     @JsonIgnore
     private List<Likes> likes;
+
+    public Comment update(CommentRequestDto requestDto){ this.content = requestDto.getContent(); return this; }
+
+    public boolean validateMember(Member member){ return !this.member.equals(member); }
 
     @Column()
     private Long likeNum;
