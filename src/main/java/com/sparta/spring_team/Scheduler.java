90a797ad3a9ec025ddc2938669aecc51901be700
Hyemm <hmlee5938@gmail.com>
2022-09-01 02:07:24 +0900

    스케쥴러 수정

--- src/main/java/com/sparta/spring_team/Scheduler.java
+++ src/main/java/com/sparta/spring_team/Scheduler.java
@@ -5,11 +5,12 @@ import com.sparta.spring_team.entity.Post;
 import com.sparta.spring_team.repository.PostRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-
+@Slf4j
 @RequiredArgsConstructor // final 멤버 변수를 자동으로 생성합니다.
 @Component // 스프링이 필요 시 자동으로 생성하는 클래스 목록에 추가합니다.
 public class Scheduler {
@@ -23,8 +24,9 @@ public class Scheduler {
         // 저장된 모든 관심상품을 조회합니다.
         List<Post> posts = postRepository.findAll();
         for (Post post : posts) {
-            if (post.getComments().size() == 0) {
+            if (post.getComments().size()==0) {
                 postRepository.delete(post);
+                log.info("게시물 <<"+post.getTitle()+">> 게시물이 삭제되었습니다");
             }
         }
     }
