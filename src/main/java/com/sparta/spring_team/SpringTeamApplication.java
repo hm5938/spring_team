ecb495d7d65ba9be32fc3369e477c0a1567359ab
Hyemm <hmlee5938@gmail.com>
2022-09-01 00:05:48 +0900

    스케쥴러 생성

--- src/main/java/com/sparta/spring_team/SpringTeamApplication.java
+++ src/main/java/com/sparta/spring_team/SpringTeamApplication.java
@@ -1,12 +1,13 @@
 package com.sparta.spring_team;
 
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableJpaAuditing
+@EnableScheduling // 스프링 부트에서 스케줄러가 작동하게 합니다.
+@EnableJpaAuditing // 시간 자동 변경이 가능하도록 합니다.
 @SpringBootApplication
 public class SpringTeamApplication {
 
